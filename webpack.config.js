e4d0c1c00f1de3dfcd704034f26b7936702e60a9
kko0831 <kko0831@hanmail.net>
2020-04-16 17:02:51 +0900

    2-9. webpack-dev-server와 hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,10 @@ module.exports = {
             ],
             "@babel/preset-react",
           ],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-hot-loader/babel",
+          ],
         },
       },
     ],
@@ -39,5 +42,6 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
+    publicPath: "/dist",
   },
 };
